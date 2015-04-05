8866ced0c4452a89676f8e249be4deba09a5383b
johnqing <csssnow@gmail.com>
2015-04-06 00:16:16 +0800

    add onpress

--- modules/List.js
+++ modules/List.js
@@ -1,5 +1,7 @@
 var React = require('react-native');
+
 var {
+  TouchableOpacity,
   StyleSheet,
   Image,
   Text,
@@ -12,6 +14,9 @@ var styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
+
+    marginBottom: 5,
+
     alignItems: 'center',
     backgroundColor: '#fff',
   },
@@ -26,6 +31,7 @@ var styles = StyleSheet.create({
   },
   rightContainer: {
     flex: 1,
+    paddingBottom: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#f1f1f1'
   },
@@ -35,29 +41,41 @@ var styles = StyleSheet.create({
   },
   artist: {
     fontSize: 12,
-  }
+  },
+  backgroundVideo: {
+    resizeMode: 'cover', // stretch and contain also supported
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
 });
 
 
-
-
 var List = React.createClass({
+    onTouch: function(url){
+      //TODO: 音乐接口未找到
+    },
     renderList: function(music){
         return (
+            <TouchableOpacity onPress={() => this.onTouch(music.url)}>
+              <View style={styles.wrap}>
+                <Image
+                  source={{uri: music.picture}}
+                  style={styles.picture}
+                />
+                <View style={styles.rightContainer}>
+                  <Text style={styles.title}>{music.title}</Text>
+                  <Text style={styles.artist}>{music.artist}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
 
-          <View style={styles.wrap}>
-            <Image
-              source={{uri: music.picture}}
-              style={styles.picture}
-            />
-            <View style={styles.rightContainer}>
-              <Text style={styles.title}>{music.title}</Text>
-              <Text style={styles.artist}>{music.artist}</Text>
-            </View>
-          </View>
         );
     },
     render: function(){
+      console.log(123);
         return <ListView
             dataSource={this.props.dataSource}
             renderRow={this.renderList}
