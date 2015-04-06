0c924dff5e13398ecd10820b3146e0aa1b7f6a0c
johnqing <csssnow@gmail.com>
2015-04-07 00:23:02 +0800

    detail

--- index.ios.js
+++ index.ios.js
@@ -2,67 +2,32 @@
 
 var React = require('react-native');
 var {
+  NavigatorIOS,
   AppRegistry,
   StyleSheet,
-  Image,
-  ListView,
-  Text,
-  View,
 } = React;
 
 
-var REQUEST_LIST_URL = 'http://www.douban.com/j/app/radio/channels';
-var REQUEST_CHANNEL_URL = 'http://douban.fm/j/mine/playlist?channel=2';
-
-// 模块集合
-var Loading = require('./modules/Loading');
-var Head = require('./modules/Head');
-var List = require('./modules/List');
+var Content = require('./modules/Content');
 
 // 启动
 var Miumiu = React.createClass({
-  
-  fetchList: function(){
-      fetch(REQUEST_CHANNEL_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(responseData.song),
-            loaded: true
-        });
-      })
-      .done();
-  },
-
-  getInitialState: function(){
-      return {
-        dataSource: new ListView.DataSource({
-            rowHasChanged: (row1, row2) => row1 !== row2,
-        }),
-        loaded: false
-      }
-  },
-  componentDidMount: function(){
-      this.fetchList();
-  },
-  
   render: function() {
-
-    if (!this.state.loaded) 
-        return <Loading />;
     return (
-        <View style={styles.container}>
-          <Head>随机推荐</Head>
-          <List dataSource={this.state.dataSource} />
-        </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: '随意听听',
+          component: Content,
+        }}
+      />
       );
   }
-
 });
 // 公共样式
 var styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    flex: 1,
     backgroundColor: '#fff',
   },
 });
--- modules/Loading.js
+++ modules/Loading.js
@@ -1,27 +1,25 @@
 var React = require('react-native');
 var {
   StyleSheet,
-  Text,
-  View,
+  ActivityIndicatorIOS,
 } = React;
 
 var styles = StyleSheet.create({
-  wrap: {
+  centering: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
-  }
+  },
 });
 
 var Loading = React.createClass({
   render: function(){
-      return <View style={styles.wrap}>
-          <Text>
-              Loading...
-          </Text>
-      </View>
+      return <ActivityIndicatorIOS
+        animating={true}
+        style={[styles.centering, {height: 80}]}
+        size="large"
+      />
   }
 });
 
